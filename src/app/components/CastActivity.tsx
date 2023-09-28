4c41583914b28663de037ce38c15f7ac131666c5
mattwelter <welter.m@icloud.com>
2023-09-27 18:03:49 -0700

    Horrible working version of cast activity

--- src/app/components/CastActivity.tsx
+++ src/app/components/CastActivity.tsx
@@ -6,7 +6,7 @@ export default async function HomeFeed(fid: any) {
   const getData = async function(){
     const data = await sql`
     SELECT DATE(timestamp) AS date,
-    COUNT(*) AS number_of_casts
+    COUNT(*) AS castAmount
     FROM casts
     WHERE casts.fid = ${fid.fid}
     GROUP BY DATE(timestamp)
@@ -17,22 +17,20 @@ export default async function HomeFeed(fid: any) {
 
   const data = await getData()
 
+  data.map((event: any) => (
+    console.log(event.castamount)
+  ))
+
 
   return (
     <>
-      <div className={style['cast-activity']}>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
+      <div className={style['cast-activity-wrapper']}>
+        <div className={style['cast-activity']}>
+          {data.length != 0 ? data.map((event: any) => (
+            <a className={ parseInt(event.castamount) == 0 ? style['cast-color-0'] : (parseInt(event.castamount) <= 2 ? style['cast-color-2'] : parseInt(event.castamount) <= 4 ? style['cast-color-4'] : parseInt(event.castamount) <= 6 ? style['cast-color-6'] : style['cast-color-8']) }></a>
+          )) : <a>Nothing here...</a>
+          }
+        </div>
       </div>
     </>
     )
