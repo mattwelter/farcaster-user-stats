a7c870e9e28d8b6f2cbd042b5f0308a3c8994427
mattwelter <welter.m@icloud.com>
2023-09-29 12:41:36 -0700

    Cast Activity is for mobile, kind of

--- src/app/components/CastActivity.tsx
+++ src/app/components/CastActivity.tsx
@@ -11,12 +11,49 @@ export default async function HomeFeed(fid: any) {
   //   263 = Thursday
   //   264 = Friday
   //   265 = Saturday
+
+  // width: 375px;
+  //   168 = Sunday
+  //   169 = Monday
+  //   170 = Tuesday
+  //   171 = Wednesday
+  //   172 = Thursday
+  //   173 = Friday
+  //   174 = Saturday
+
+  let getDay = new Date().getDay()
+  var day = 0
+  switch (getDay) {
+    case 0: // Sunday
+      var day = 168
+      break;
+    case 1: // Monday
+      var day = 169
+      break;
+    case 2: // Tuesday
+      var day = 170
+      break;
+    case 3: // Wednesday
+      var day = 171
+      break;
+    case 4: // Thursday
+      var day = 172
+      break;
+    case 5: // Friday
+      var day = 173
+      break;
+    case 6: // Saturday
+      var day = 174
+      break;
+    default:
+      var day = 171 // Wednesday default
+  }
   
   const getData = async function(){
     const data = await sql`
       WITH date_range AS (
         SELECT NOW() - (n || ' days')::interval AS date
-        FROM generate_series(0, 262) AS n
+        FROM generate_series(0, ${day}) AS n
       )
       SELECT DATE(date_range.date) AS date,
             COUNT(casts.timestamp) AS castamount
