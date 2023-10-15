e0068aea5863ded10b3350e0cc8e05a24e18af35
mattwelter <welter.m@icloud.com>
2023-10-15 15:08:31 -0700

    Reverted "User not found"

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -13,14 +13,13 @@ export default function Page() {
 
     if (!res.result) {
       return alert('No user found')
-    }
-
-    try {
-      const user = res.result.user
-      return redirect(`/users/${res.result.user.fid}`)
-    } catch {
+    } else if (res.code == "NotFound") {
       return alert('No user found')
     }
+
+    const user = res.result.user
+
+    return redirect(`/users/${res.result.user.fid}`)
   }
 
   return (
