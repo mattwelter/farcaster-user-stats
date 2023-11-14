271ce2c7dc0577c113c2bcb330ceda522434c5fe
mattwelter <welter.m@icloud.com>
2023-11-13 21:35:11 -0700

    Trending link on user page

--- src/app/components/GetRanking.tsx
+++ src/app/components/GetRanking.tsx
@@ -71,7 +71,7 @@ export default async function HomeFeed(fid: any) {
 
     return (
         <>
-            <h2 className={style.rank}>&nbsp;• { rank_data.length != 0 ? "Rank #" + rank_data[0].rank + " of " + rank_data[0].total_count : "Unranked" }</h2>
+            <h2 className={style.rank}>&nbsp;• <a href={'/trending'}>{ rank_data.length != 0 ? "Rank #" + rank_data[0].rank + " of " + rank_data[0].total_count : "Unranked" }</a></h2>
         </>
     )
 }
