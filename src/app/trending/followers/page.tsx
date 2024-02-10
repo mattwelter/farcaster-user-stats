23eccf06ffe79ecb05a0d537ada134abbab45335
mattwelter <welter.m@icloud.com>
2024-02-09 17:50:18 -0700

    Turning site off again

--- src/app/trending/followers/page.tsx
+++ src/app/trending/followers/page.tsx
@@ -40,8 +40,8 @@ export default async function Page() {
                     </section>
                 </div>
             </div>
-            {/* <p>You guys broke my site. Site down for now</p> */}
-            <div className={`${'navigation'} ${style['on-leaderboard-page']}`}>
+            <p>You guys broke my site. Site down for now</p>
+            {/* <div className={`${'navigation'} ${style['on-leaderboard-page']}`}>
                 <nav>
                     <ul>
                         <li><a href="/trending/ratio">Trending</a></li>
@@ -53,7 +53,7 @@ export default async function Page() {
 
             <Suspense fallback={<MostFollowedUsersLoading />}>
                 <PopularUsers />
-            </Suspense>
+            </Suspense> */}
         </main>
     )
 }
