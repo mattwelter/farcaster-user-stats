d80caf81d6701e394648cf18057d87c2cd82d61e
mattwelter <welter.m@icloud.com>
2024-02-10 20:20:07 -0700

    Under maintenance

--- src/app/users/[fid]/page.tsx
+++ src/app/users/[fid]/page.tsx
@@ -50,15 +50,15 @@ export default async function Page({ params }: {
     params: { fid: string }
 }) {
 
-    const getUser = await fetch(`https://api.neynar.com/v1/farcaster/user/?api_key=${process.env.NEYNAR_API_KEY}&fid=${params.fid}&viewerFid=3`, { method: "GET" });
-    const userResponse = await getUser.json();
-    let user = userResponse.result.user;
-    console.log({ user })
+    // const getUser = await fetch(`https://api.neynar.com/v1/farcaster/user/?api_key=${process.env.NEYNAR_API_KEY}&fid=${params.fid}&viewerFid=3`, { method: "GET" });
+    // const userResponse = await getUser.json();
+    // let user = userResponse.result.user;
+    // console.log({ user })
 
     return (
         <main className={style['top-bottom-padding']}>
-            {/* <p>Site is under maintenance, please <a className={'white'} href="https://warpcast.com/fun">follow on Farcaster</a> for updates.</p> */}
-            <div className={style['user-page-header']}>
+            <p>Site is under maintenance, please <a className={'white'} href="https://warpcast.com/fun">follow on Farcaster</a> for updates.</p>
+            {/* <div className={style['user-page-header']}>
                 <div className="width-500">
                     <section>
                         <a className="svg-back-button-wrapper" href="/">
@@ -72,9 +72,9 @@ export default async function Page({ params }: {
                         <img className="profile-pic" src={user.pfp.url ? user.pfp.url : "/avatar.png"} height="48px" width="48px" />
                         <h1>{ user ? user.displayName : params.fid }</h1>
                         <h2>{ user ? "@" + user.username : params.fid }</h2>
-                        {/* <Suspense fallback={<a className={`${style['rank-loading']}`}>&nbsp;• Loading...</a>}>
+                        <Suspense fallback={<a className={`${style['rank-loading']}`}>&nbsp;• Loading...</a>}>
                             <GetRanking fid={params.fid} />
-                        </Suspense> */}
+                        </Suspense>
                     </div>
                 </div>
             </div>
@@ -131,7 +131,7 @@ export default async function Page({ params }: {
                         </Suspense>
                     </div>
                 </div>
-            </Body>
+            </Body> */}
         </main>
     )
 }
