abf30f2dfc506d15f4ebee1c404744a2d1aa3eea
mattwelter <welter.m@icloud.com>
2024-02-10 19:11:40 -0700

    It's broken

--- src/app/trending/ratio/page.tsx
+++ src/app/trending/ratio/page.tsx
@@ -45,8 +45,8 @@ export default async function Page() {
 
                 </div>
             </div>
-            {/* <p>You guys broke my site. Site down for now</p> */}
-            <div className={`${'navigation'} ${style['on-leaderboard-page']}`}>
+            <p>Site is under maintenance</p>
+            {/* <div className={`${'navigation'} ${style['on-leaderboard-page']}`}>
                 <nav>
                     <ul>
                         <li><a href="/trending/ratio">Trending</a></li>
@@ -58,7 +58,7 @@ export default async function Page() {
 
             <Suspense fallback={<PopularUsersLoading />}>
                 <PopularUsers />
-            </Suspense>
+            </Suspense> */}
         </main>
     )
 }
