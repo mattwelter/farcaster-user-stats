8f4b483a09190c6b815b23dd4d198351918f3ea9
mattwelter <welter.m@icloud.com>
2024-02-15 23:32:59 -0700

    Fixed fid mapping issue

--- src/app/api/daily-stats/route.js
+++ src/app/api/daily-stats/route.js
@@ -10,7 +10,7 @@ export async function GET(request) {
     }
 
     try {
-        const cacheKey = `dailystats:${fid.fid}`;
+        const cacheKey = `dailystats:${fid}`;
         let cachedData = await redis.get(cacheKey);
     
         if (cachedData) {
