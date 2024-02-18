1995477b2b35b90ca111469f220bc8ed0de7e261
mattwelter <welter.m@icloud.com>
2024-02-17 22:36:25 -0700

    Added subdomain www. to all internal API requests

--- src/app/components/MostLikedCasts.tsx
+++ src/app/components/MostLikedCasts.tsx
@@ -2,7 +2,7 @@ import MostLikedCasts from './MostLikedCastsClient'
 
 export default async function UserFeed(fid: any, username: any) {
   async function getTotalLikedCasts() {
-    const response = await fetch(`https://farcasteruserstats.com/api/most-liked-casts?fid=${fid.fid}`);
+    const response = await fetch(`https://www.farcasteruserstats.com/api/most-liked-casts?fid=${fid.fid}`);
     if (!response.ok) { throw new Error('Failed to fetch 7 day follower summary'); }
     let data = await response.json()
     return data
